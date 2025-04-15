731c7578a51519e99fec863822a86593277bfd08
Adesh Nale <129063448+adesh17@users.noreply.github.com>
2025-04-15 08:58:19 +0530

    Time: 388 ms (59.07%), Space: 68.2 MB (12.23%) - LeetHub

---
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    
+    result = pd.merge(employees, employee_uni[['id', 'unique_id']], on='id', how='left')
+    
+    
+    return result[['unique_id', 'name']]
