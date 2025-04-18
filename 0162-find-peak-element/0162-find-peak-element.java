c8619614af3a8b4648108d978cadd2a4a16a4bd6
Adesh Nale <129063448+adesh17@users.noreply.github.com>
2025-04-18 14:47:40 +0530

    Time: 0 ms (100%), Space: 42.6 MB (43.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,13 +3,6 @@ class Solution {
     {
         int index=0,max=nums[0];
 
-        if(nums.length==2)
-        {
-            if(nums[1]>nums[0])
-            {
-                return 1;
-            }
-        }
         for(int i=1;i<nums.length;i++)
         {
             if(nums[i]>max)
